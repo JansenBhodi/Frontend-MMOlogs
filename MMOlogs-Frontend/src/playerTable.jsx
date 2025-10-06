fe6066bdbef81e3b3d0ecbbdf6c2c4ed18cb939d
JansenBhodi <117980774+JansenBhodi@users.noreply.github.com>
2025-10-06 14:46:09 +0200

    main functionality player view
    Created the main functionality with a data grid for viewing all players

---
+++ MMOlogs-Frontend/src/playerTable.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react'
+import { DataGrid } from '@mui/x-data-grid'
+import Paper from '@mui/material/Paper'
+import App from './App.jsx'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+
+
+const PlayerTable = () => {
+
+  const [count, setCount] = useState(0) 
+  const [players, setPlayers] = useState([])
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  
+  const columns = [
+    { field: 'name', headerName: 'Character Name', width: 130 },
+    { field: 'roleclass', headerName: 'Class', width: 100 },
+  ]
+
+  const paginationModel = { page: 0, pageSize: 5 };
+  const roleclassMap = {
+    0: 'Warrior',
+    1: 'Knight',
+    2: 'Fighter',
+    3: 'Archer',
+    4: 'Fire Mage',
+    5: 'Scholar',
+    6: 'Cleric'
+
+  }
+
+  const RedirectToPlayer = (params) => {
+    const playerName = params.row.name
+    navigate(`/players/${playerName}`)
+  }
+
+  useEffect(() => {
+    const fetchData = () => {
+      setLoading(true)
+      axios.get('https://localhost:7289/Player')
+        .then(function (response) {
+          setPlayers(response.data.data)
+          console.log(response.data.data)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+        .finally(function () {
+          setLoading(false)
+        })
+    }
+
+
+  //Lets start with a filled table
+  fetchData()
+
+  //Pull the data again every minute
+  const Interval = setInterval(fetchData, 60000)
+  //prevent memory leakage
+  return () => clearInterval(Interval)
+
+  }, [])
+
+  return (
+    <Paper sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={players}
+        columns={columns}
+        initialState={{ pagination: { paginationModel } }}
+        pageSizeOptions={[5, 10]}
+        onRowClick={RedirectToPlayer}
+        sx={{ border: 0 }}
+      />
+    </Paper>
+  )
+}
+
+export default PlayerTable
