4bf204c22b07e1469f93901bc3a37c70680b46b0
JansenBhodi <117980774+JansenBhodi@users.noreply.github.com>
2025-11-10 11:05:57 +0100

    finish current testing

--- MMOlogs-Frontend/cypress/e2e/players.cy.js
+++ MMOlogs-Frontend/cypress/e2e/players.cy.js
@@ -55,7 +55,49 @@ describe('player registry', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5173/players/registry')
   });
-  it('successful registry of player', () => {
 
+    it('can we load registry', () => {
+    cy.contains('Player register form.').should('exist');
+    cy.get('input[type="text"]').should('exist');
+    cy.get('select').should('exist');
+    cy.get('button[type="submit"]').should('contain', 'Register');
+  });
+
+  it('successful registry of player', () => {
+    //use a date.now since we need the name to be unique
+    //it saves it to the real backend after all
+    const playerName = `TestHero_${Date.now()}`;
+    cy.get('input[type="text"]').type(playerName);
+    cy.get('select').select('Warrior'); 
+    cy.get('button[type="submit"]').click();
+
+    
+    cy.url({ timeout: 10000 }).should('include', `/players/${playerName}`);
+    cy.contains(playerName).should('exist'); 
+  });
+  
+  it('try to leave player name empty', () => {
+  cy.intercept('POST', '**/Player').as('createPlayer');
+
+  cy.get('select').select('Knight');
+  cy.get('button[type="submit"]').click();
+
+
+  cy.wait(1000);
+  cy.get('@createPlayer.all').should('have.length', 0);
+
+  cy.get('input:invalid').should('have.length', 1);
+  });
+  
+  it('try to not select a class', () => {
+  cy.intercept('POST', '**/Player').as('createPlayer');
+
+  cy.get('input[type="text"]').type('LonelyHero');
+  cy.get('button[type="submit"]').click();
+
+  cy.wait(1000);
+  cy.get('@createPlayer.all').should('have.length', 0);
+
+  cy.get('select:invalid').should('have.length', 1);
   });
 });
